3254d8381e544cb11f3ac9fd1d51e1c58fac961f
abdulwahab hassan <abdulwahabhassan@abdulwahabs-MacBook-Pro.local>
2026-09-02 12:26:04 -0700

    Add Intro.cpp

---
+++ Module01-Setup/Intro.cpp
@@ -0,0 +1,11 @@
+
+//This is the begining of the Intro.cpp file
+#include <iostream>
+using namespace std;
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    cout << "My name is Abdulwahab Hassan";
+    return 0;
+}
+
+
